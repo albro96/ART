e41e39331c8380184664d0b262f69e3106512bfe
Alexander Broll <alexbroll1302@gmail.com>
2024-02-23 14:35:55 +0100

    wandb sweep stuff

--- bra/rotenc_train.py
+++ bra/rotenc_train.py
@@ -11,6 +11,7 @@ from collections import Counter
 import wandb
 import hashlib
 import json
+import math
 
 BASE_DIR = Path(op.abspath(__file__)).parents[1].resolve()
 sys.path.append(str(BASE_DIR))
@@ -37,23 +38,53 @@ torch.manual_seed(seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = True
 
-opt = EasyDict()
-opt.size = 128
-opt.art = True
-opt.resume = False
-opt.iters = 5
-opt.lambda_mse = 2
-opt.lambda_cd = 50
-opt.resamplemode = 'fps'
-opt.num_points = 4096
-opt.cache_data = True
-opt.batch_size = 16
-opt.num_workers = 16
-opt.epoch_save = 50
-opt.val_step = 1
-opt.end_epoch = 500 
-opt.lr = 1e-4
-opt.data_dir = '/storage/share/nobackup/data/ShapeNet55-34/shapenet_pc'
+def parse_args():
+    parser = argparse.ArgumentParser(description='Training options')
+
+    parser.add_argument('--size', type=int, default=128)
+    parser.add_argument('--art', type=bool, default=True)
+    parser.add_argument('--resume', type=bool, default=False)
+    parser.add_argument('--iters', type=int, default=5)
+    parser.add_argument('--lambda_mse', type=int, default=2)
+    parser.add_argument('--lambda_cd', type=int, default=50)
+    parser.add_argument('--resamplemode', type=str, default='fps')
+    parser.add_argument('--num_points', type=int, default=4096)
+    parser.add_argument('--cache_data', type=bool, default=True)
+    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--num_workers', type=int, default=16)
+    parser.add_argument('--epoch_save', type=int, default=50)
+    parser.add_argument('--val_step', type=int, default=1)
+    parser.add_argument('--end_epoch', type=int, default=500)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--data_dir', type=str, default='/storage/share/nobackup/data/ShapeNet55-34/shapenet_pc')
+    parser.add_argument('--category', type=str, default='02691156')
+
+    return EasyDict(parser.parse_args())
+
+opt = parse_args()
+
+if opt.iters > 5:
+    opt.batch_size = 8
+
+# opt = EasyDict()
+
+# opt.size = 128
+# opt.art = True
+# opt.resume = False
+# opt.iters = 5
+# opt.lambda_mse = 2
+# opt.lambda_cd = 50
+# opt.resamplemode = 'fps'
+# opt.num_points = 4096
+# opt.cache_data = True
+# opt.batch_size = 16
+# opt.num_workers = 16
+# opt.epoch_save = 50
+# opt.val_step = 1
+# opt.end_epoch = 500 
+# opt.lr = 1e-4
+# opt.data_dir = '/storage/share/nobackup/data/ShapeNet55-34/shapenet_pc'
+# opt.category = '02691156' 
 
 # get all categories from os.listdir(opt.data_dir) as path.split('-')[0] and make unique list
 files = os.listdir(opt.data_dir)
@@ -69,24 +100,41 @@ for category in categories:
 data_paths = glob.glob(op.join(opt.data_dir, f'{categories[0]}*.npy'))
 dataset = 'ShapeNet'
 
-for category in categories:
-    opt.category = category #  #categories[9] # '02691156'
-    # create unique hash from opt
-    opt_str = json.dumps(opt, sort_keys=True)
-    opt_hash = hashlib.sha256(opt_str.encode()).hexdigest()[:8]
+suffix = f''
 
-    print(f'opt_hash: {opt_hash}')
- 
-    # runname with date in format YYMMDD from datetime function
-    runname = f'{datetime.now().strftime("%y%m%d")}_category-{opt.category}-{opt_hash}'
+# create unique hash from opt
+opt_str = json.dumps(opt, sort_keys=True)
+opt_hash = hashlib.sha256(opt_str.encode()).hexdigest()[:8]
 
-    save_dir = op.join(model_dir, dataset, runname)
+print(f'opt_hash: {opt_hash}')
 
-    os.makedirs(save_dir, exist_ok=True)
+# runname with date in format YYMMDD from datetime function
+runname = f'{datetime.now().strftime("%y%m%d")}_category-{opt.category}-{opt_hash}'
 
-    # save json
-    with open(op.join(save_dir, 'config.json'), 'w') as f:
-        json.dump(opt, f, indent=4)
+runname += suffix
+
+save_dir = op.join(model_dir, dataset, runname)
+
+os.makedirs(save_dir, exist_ok=True)
+
+# save json
+with open(op.join(save_dir, 'config.json'), 'w') as f:
+    json.dump(opt, f, indent=4)
+
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="ART",
+    # track hyperparameters and run metadata
+    config=opt,
+    name=f'{dataset}-{runname}',
+    dir=save_dir,
+    save_code=True,
+)
+
+with wandb.init(config=opt):
+    # If called by wandb.agent, as below,
+    # this config will be set by Sweep Controller
+    opt = wandb.config
 
     # ----------------------------- Prepare data -----------------------------
     train_set = ShapeNet_PC(
@@ -134,15 +182,6 @@ for category in categories:
 
 
 
-    wandb.init(
-        # set the wandb project where this run will be logged
-        project="ART",
-        # track hyperparameters and run metadata
-        config=opt,
-        name=f'{dataset}-{runname}',
-        dir=save_dir,
-        save_code=True,
-    )
 
     # define our custom x axis metric
     wandb.define_metric("train/epoch")
